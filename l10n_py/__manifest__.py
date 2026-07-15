d6bfc961905f876cab834a5a53f91e268b8eae45
FCubilla77 <f.cubilla77@gmail.com>
2026-07-15 16:56:09 -0300

    Actualización l10n_py 005

--- l10n_py/__manifest__.py
+++ l10n_py/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Paraguay - Contabilidad",
-    "version": "19.0.2026.0004",
+    "version": "19.0.2026.0005",
     "category": "Accounting/Localizations/Account Charts",
     "license": "LGPL-3",
     "author": "FC_Py",
