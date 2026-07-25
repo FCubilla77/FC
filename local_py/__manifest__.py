f77fa9d82e6888f4cec2571739a21e312b5149d0
FCubilla77 <f.cubilla77@gmail.com>
2026-07-25 03:38:52 -0300

    Actualización local_py 2026.05.004

--- local_py/__manifest__.py
+++ local_py/__manifest__.py
@@ -19,7 +19,7 @@
     "author": "FC_Py",
     "website": "http://www.www.com.py",
     "category": "Localización",
-    "version": "2026.05.003",
+    "version": "2026.05.004",
     "license": "LGPL-3",
     "depends": ["base", "account", "base_address_extended", "mail"],
     "data": [
