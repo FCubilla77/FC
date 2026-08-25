4ab151fa77827bce23a92e5c9c1955ddb72a488c
FCubilla77 <f.cubilla77@gmail.com>
2026-08-25 20:21:38 -0300

    Actualización FE_Py 2026.01.017

--- FE_Py/__manifest__.py
+++ FE_Py/__manifest__.py
@@ -25,7 +25,7 @@
     "author": "FC_Py",
     "website": "http://www.www.com.py",
     "category": "Localización",
-    "version": "2026.01.016",
+    "version": "2026.01.017",
     "license": "LGPL-3",
     "depends": ["local_py"],
     "external_dependencies": {
