9d0ee870be38758b4d70ec544d9df206342e7d62
FCubilla77 <f.cubilla77@gmail.com>
2026-08-28 13:29:49 -0300

    Actualización FE_Py 2026.01.032

--- FE_Py/models/__init__.py
+++ FE_Py/models/__init__.py
@@ -6,10 +6,10 @@ from . import fe_py_documento_electronico_signing
 from . import fe_py_documento_electronico_envio
 from . import fe_py_documento_electronico_kude
 from . import fe_py_documento_electronico_email
-from . import fe_py_cron
 from . import local_py_documento_anulado
 from . import fe_py_evento
 from . import fe_py_evento_procesamiento
+from . import fe_py_cron
 from . import res_company
 from . import account_journal
 from . import local_py_tipo_fiscal
